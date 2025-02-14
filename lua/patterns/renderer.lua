516286a807b631c43136b606c47c095dd5dc98e5
Shawon <mdmoinulhossainshawon@gmail.com>
2025-02-14 18:00:16 +0600

    feat(renderer): Renderer now returns the line number of the curren't nodes preview
    Used for changing cursor position.

--- lua/patterns/renderer.lua
+++ lua/patterns/renderer.lua
@@ -4,13 +4,16 @@ renderer.lua_patterns = require("patterns.renderers.lua_patterns");
 renderer.regex = require("patterns.renderers.regex");
 
 renderer.render = function (buffer, content)
+	local current_line;
+
 	for k, v in pairs(content) do
 		if renderer[k] then
-			renderer[k].render(buffer, v);
+			current_line = renderer[k].render(buffer, v);
 		end
 	end
 
 	vim.api.nvim_buf_set_lines(buffer, 0, 1, false, {});
+	return current_line;
 end
 
 return renderer;
